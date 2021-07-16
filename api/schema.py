fe4313917a3d341512a513f6590638a23676025f
iameo <okwudike.emmanuel@gmail.com>
2021-07-16 14:18:08 +0100

    db schema

--- api/schema.py
+++ api/schema.py
@@ -3,21 +3,26 @@ from pydantic import BaseModel, Field
 
 from datetime import datetime
 
+from api.db import Song, Album
+
+
+current_year = int(datetime.now().year)
 
 
 class MusicBase(BaseModel):
     title: Optional[str] = None
-    artist: Optional[str] = None
-    audio_file: Optional[str] = None
+    artist: Optional[int] = 1
+    audio_file: Optional[bytes] = None
+
 
 
 class MusicDetail(MusicBase):
-    cover_image: str
-    album: str
-    audio_length: str
+    cover_image: Optional[bytes] = None
+    # album_id: Optional[int] = None
+    # audio_length: Optional[str] = '00:00'
 
 
-class MusicCreate(MusicBase):
+class MusicCreate(MusicDetail):
     pass
 
 class MusicSchema():
@@ -32,7 +37,7 @@ class MusicSchema():
                 "title": "Fret Not",
                 "artist": "Mr Alchemy",
                 "audio_file": "fret_not.mp3",
-                "cover_image": "fret-not.jpg",
+                "cover_image": "fret-not-cover-image.jpg",
                 "audio_length": "3:23",
                 "uploaded_at": "2021-04-23T02:16:45"
             }
@@ -40,7 +45,7 @@ class MusicSchema():
 
 
 class MusicOut(MusicDetail):
-    created_at: datetime
+    uploaded_at: datetime
 
     class Config:
         orm_mode = True
@@ -54,6 +59,32 @@ class MusicOut(MusicDetail):
                 "uploaded_at": "2021-04-23T02:16:45"
             }
         }
+
+
+##################### ALBUM ########################################
+
+class AlbumBase(BaseModel):
+    name: Optional[str] = 'AlbumName'
+    artist: Optional[int] = 1
+    cover_image: Optional[bytes] = None
+    tracks: Optional[List[Song]] = [None]
+    production_year: Optional[int] = current_year
+
+
+class AlbumCreate(AlbumBase):
+    pass
+
+
+
+######################## ARTIST ###############################
+
+class ArtistBase(BaseModel):
+    name: Optional[str] = 'ArtistName'
+    record_label: Optional[str] = 'ArtistRecordLabel'
+
+
+# class ArtistOut(BaseModel):
+#     pass
 
 
 
