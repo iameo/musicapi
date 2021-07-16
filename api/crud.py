1e388a94017e1dfefc43f66ad75c5ec904c7dff9
iameo <okwudike.emmanuel@gmail.com>
2021-07-16 14:16:36 +0100

    crud

--- api/crud.py
+++ api/crud.py
@@ -2,7 +2,7 @@
 
 from api import schema
 
-from api.db import Song, Album
+from api.db import Song, Album, Artist
 
 
 
@@ -25,6 +25,23 @@ async def get_songs(skip: int = 0, limit: int = 10):
     return await songs
 
 
+# async def get_song_detail(artist, title):
+#     query = Song.objects().where(Song.c.artist==artist).where(song.c.title==title)
+#     return await database.fetch_one(query=query)
+
+# async def delete_song(id: int):
+#     query = Song.delete().where(Song.c.id==id)
+#     return await database.execute(query=query)
+
+# async def update_song(id: int, payload: schema.MusicCreate): #I can't think of a possible usecase for now
+#     query = (
+#         Song
+#         .update()
+#         .where(Song.c.id == id)
+#         .values(**payload.dict())
+#     )
+#     return await database.execute(query=query)
+
 
 # ######################## ALBUM ################################
 
@@ -44,3 +61,18 @@ async def get_albums_by_name(name: str):
 async def get_albums(skip: int = 0, limit: int = 10):
     albums = Album.objects.order_by(Album.id.desc()).offset(skip).limit(limit).all()
     return await albums
+
+async def get_album_detail(name, artist):
+    album = Album.objects.filter(name=name, artist=artist)
+    return await album
+
+
+################ ARTIST #########################
+
+async def add_artist(payload: schema.ArtistBase):
+    artist = Artist.objects.create(**payload.dict())
+    return await artist
+
+async def get_artist():
+    artist = Artist.objects.all()
+    return await artist
